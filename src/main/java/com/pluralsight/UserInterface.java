7b88ea5c6f115e3ded243e5856e4a0a6326e492a
Steven <stevccru@icloud.com>
2023-11-27 14:36:34 -0500

    updated

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -41,6 +41,8 @@ public class UserInterface {
         System.out.println("[1] add sandwich");
         System.out.println("[2] add drink");
         System.out.println("[3] add chips");
+        System.out.println("[4] checkout");
+        System.out.println("[0] cancel order");
 
         int selection = keyboard.nextInt();
 
@@ -65,14 +67,15 @@ public class UserInterface {
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("What size sandwich in would you like; 4inch, 8inch, or 12 inch?");
-        int size = keyboard.nextInt();
+        int sandwichSize = keyboard.nextInt();
 
         System.out.println("What type of bread would you like?");
         String breadType = keyboard.next().trim().toUpperCase();
 
+        //Prompt user for toppings to use
 
 
-
+        System.out.println();
 
     }
 
@@ -80,7 +83,10 @@ public class UserInterface {
 
 
     public static void addDrink(){
+        Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("What size drink would you like?");
+        String drinkSize = keyboard.next().toUpperCase().trim();
 
 
     }
