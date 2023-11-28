2c0688db8765333c7862a1ca5cd18ac76e5b46f7
Andi <alexandra.e.burke@gmail.com>
2023-11-28 10:53:24 -0500

    getCart and Total methods

--- src/main/java/com/pluralsight/Item.java
+++ src/main/java/com/pluralsight/Item.java
@@ -4,5 +4,21 @@ import java.util.ArrayList;
 
 public abstract class Item implements ItemFunctions{
     ArrayList<Item> cart;
+    public Item() {
+        this.cart = new ArrayList<>();
+    }
+
+    public static ArrayList<Item> getCart() {
+        return cart;
+    }
+    public static double getTotal() {
+        double total = 0.0;
+        for (Item item : cart){
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+
 
 }
